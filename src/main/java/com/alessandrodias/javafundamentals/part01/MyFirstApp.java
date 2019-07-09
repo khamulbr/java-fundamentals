592ff117d9be737b2b2e67f6bbb52d6eb7f10ab6
Alessandro <alessandro.valerio.dias@gmail.com>
2019-07-09 00:47:58 -0300

    First Class created

---
+++ src/main/java/com/alessandrodias/javafundamentals/part01/MyFirstApp.java
@@ -0,0 +1,12 @@
+package com.alessandrodias.javafundamentals.part01;
+
+// code based on Head First: Java examples
+// by Sierra and Bates, 2010
+
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		System.out.println("I Rule!");
+	}
+
+}
